f881dcc34a90f1d2c1475b0d7e0331333d258fbf
Saurabh Bhalerao <saurabhbhalerao1989@gmail.com>
2025-09-04 23:26:24 +0530

    Updated dark mode components

--- app/root.tsx
+++ app/root.tsx
@@ -26,7 +26,7 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -34,7 +34,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<Links />
 			</head>
 			<body>
-				<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
 					{children}
 				</ThemeProvider>
 				<ScrollRestoration />
